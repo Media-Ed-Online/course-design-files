3a35a43f056cf7f38d98f5d4013acd7645e7aefc
JustineEvansUM <justine.evans@umontana.edu>
2021-08-20 20:08:41 -0600

    added first batch of files

---
+++ svma/intro-animation/intro-animation-01.js
@@ -0,0 +1,1 @@
+$('#region-main').prepend('<img src="https://video-public.canva.com/VADls-1PCW0/videos/e9ebf767df.gif" style="width: 200px; height: 200px; position: absolute; bottom: -100px; left: -100px;"/>');
