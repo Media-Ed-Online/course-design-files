c76c4037ed069cabf05d2a871698eeee3577426b
JustineEvansUM <justine.evans@umontana.edu>
2021-08-27 14:18:04 -0600

    added tab jsf

--- svma/intro-animation/intro-animation-01.js
+++ svma/intro-animation/intro-animation-01.js
@@ -1,1 +1,10 @@
 //$('#region-main').prepend('<img src="https://video-public.canva.com/VADls-1PCW0/videos/e9ebf767df.gif" style="width: 200px; height: 200px; position: absolute; bottom: -100px; left: -100px;"/>');
+
+
+$(document).ready(function(index) {
+  $('li.modtype_label .activityinstance .snap-header-card').css("height", "0px");
+  $('li.modtype_label .activityinstance .snap-assettype').css("height", "0px");
+  $('li.modtype_label:first-of-type').contents().appendTo('#tab-container div.tab-content div#nav-first');
+  $('li.modtype_label:nth-of-type(2)').contents().appendTo('#tab-container div.tab-content div#nav-second');
+  $('li.modtype_label:nth-of-type(3)').contents().appendTo('#tab-container div.tab-content div#nav-third');
+});
