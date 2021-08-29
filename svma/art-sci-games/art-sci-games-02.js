250986f486232e11b793606fe05785ca8d7ae9f7
JustineEvansUM <justine.evans@umontana.edu>
2021-08-29 01:47:14 -0600

    art sci games tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -33,13 +33,12 @@ $(document).ready(function() {
 | URL MODULE GROUP   |
 ------------------ */
 $(document).ready(function(index) {
+  var URL = "li.modtype_url"
+  var BOOK = "li.modtype_book"
+  var H5P = "li.modtype_h5pactivity"
 
   // select only items titled Level...
-  if ($(this).find('p.instancename:contains("Tutorial")'').length > 0) {
-
-    let URL = "li.modtype_url"
-    let BOOK = "li.modtype_book"
-    let H5P = "li.modtype_h5pactivity"
+  if ($("li.modtype_book").find('p.instancename:contains("Tutorial")').length > 0) {
 
     //-- CHANGE THE URL MODTYPE TITLE
     $(URL).each(function(index) {
@@ -113,8 +112,5 @@ $(document).ready(function(index) {
       'height': '30px',
       'font-size': '30px',
     });
-  }
-  else {
-        // Do nothing
-  }
+  };
 });
