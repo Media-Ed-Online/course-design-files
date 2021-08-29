4c553b5a9195bab65c77a38ec5dc9018e6c5eefe
JustineEvansUM <justine.evans@umontana.edu>
2021-08-29 11:25:17 -0600

    art sci games tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -1,42 +1,18 @@
 /* ------------------
-| TABLE OF CONTENTS  |
------------------- */
-/*$("nav#course-toc h2").each(function() {
-var text = $(this).text();
-text = text.replace("Contents", "Levels");
-$(this).text(text);
-});*/
-
-
-
-/* ------------------
-| BACKGROUND         |
------------------- */
-$(document).ready(function() {
-  // Draw Shapes
-  var $backgroundShapes = $("<div class='bgShapes'><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></div>")
-  $("#page").prepend($backgroundShapes);
-
-  $("div.bgShapes span").each(function() {
-    // Random BG Colors
-    var bgShapesColorOpts = ["#DF3D9B", "#FDC273", "#0573CD"];
-    var bgShapesColorRand = bgShapesColorOpts[Math.floor(Math.random() * bgShapesColorOpts.length)];
-    console.log(bgShapesColorRand);
-    $(this).css('background', bgShapesColorRand);
-  });
-});
-
-
-
-
-/* ------------------
 | URL MODULE GROUP   |
 ------------------ */
-$('li.modtype_url:contains("tutorial"), li.modtype_url:contains("tutorial")').each(function(index) {
-  // variables
-  let oldTitle = $(this).find(".snap-asset-link")
-  let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-  // save the title, remove it, and add to new location
-  oldTitle.remove();
-  newTitle.append(oldTitle);
+$(document).ready(function(index) {
+  var textTitle = "Tutorial"
+  var URL = $('li.modtype_url:contains(textTitle)');
+  var BOOK = $('li.modtype_book:contains(textTitle)');
+  var H5P = $('li.modtype_h5pactivity:contains(textTitle)');
+
+  $(URL).each(function(index) {
+    // variables
+    let oldTitle = $(this).find(".snap-asset-link")
+    let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+    // save the title, remove it, and add to new location
+    oldTitle.remove();
+    newTitle.append(oldTitle);
+  });
 });
