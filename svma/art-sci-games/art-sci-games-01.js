63c0a6924dbfe833b49b3a42bfa5a02e322861ba
JustineEvansUM <justine.evans@umontana.edu>
2021-08-28 20:10:32 -0600

    art sci games bg

--- svma/art-sci-games/art-sci-games-01.js
+++ svma/art-sci-games/art-sci-games-01.js
@@ -1,7 +1,7 @@
 /* ------------------
 | TABLE OF CONTENTS  |
  ------------------ */
-/*$("nav#course-toc h2").each(function() {
+$("nav#course-toc h2").each(function() {
     var text = $(this).text();
     text = text.replace("Contents", "Levels");
     $(this).text(text);
@@ -11,4 +11,3 @@ var $backgroundCircles = $( "<ul class='circles'><li></li><li></li><li></li><li>
 
 $( "#moodle-page" ).append( $backgroundCircles );
 //$( "#moodle-page ul.circles" ).height($(document).height());
-*/
