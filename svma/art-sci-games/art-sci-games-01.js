d2f283dcb8814ed80a4c56953f65327d77ef761b
JustineEvansUM <justine.evans@umontana.edu>
2021-08-30 03:14:23 -0600

    performance check

--- svma/art-sci-games/art-sci-games-01.js
+++ svma/art-sci-games/art-sci-games-01.js
@@ -64,16 +64,22 @@ $(document).ready(function(index) {
         // relocated all moved-targeted items to new location
         newIconLocation.append(oldIcon_URL).append(oldIcon_H5P).append(oldIcon_BOOK);
       });
+      let oldIcon_URL = $("li.modtype_url:not(.moved)").first().find(oldIcon).addClass("moved")
+      let oldIcon_BOOK = $("li.modtype_book:not(.moved)").first().addClass("moved").find(oldIcon)
+      let oldIcon_H5P = $("li.modtype_h5pactivity:not(.moved)").first().addClass("moved").find(oldIcon)
+      let newIconLocation = $("li.modtype_url:not(.moved)").first().addClass("moved").find(newIcon)
+      // relocated all moved-targeted items to new location
+      newIconLocation.append(oldIcon_URL).append(oldIcon_BOOK).append(oldIcon_H5P);
       //next of each modtype, repeated
-      //setInterval(function() {
+      /*setInterval(function() {
         // find the first non-targeted of each, add empty class to mark
         let oldIcon_URL = $("li.modtype_url:not(.moved)").first().find(oldIcon).addClass("moved")
         let oldIcon_BOOK = $("li.modtype_book:not(.moved)").first().addClass("moved").find(oldIcon)
         let oldIcon_H5P = $("li.modtype_h5pactivity:not(.moved)").first().addClass("moved").find(oldIcon)
         let newIconLocation = $("li.modtype_url:not(.moved)").first().addClass("moved").find(newIcon)
         // relocated all moved-targeted items to new location
-        newIconLocation.append(oldIcon_URL).append(oldIcon_BOOK).append(oldIcon_H5P//);
-      });
+        newIconLocation.append(oldIcon_URL).append(oldIcon_BOOK).append(oldIcon_H5P);
+      });*/
     });
 
     //-- Styling Changes
