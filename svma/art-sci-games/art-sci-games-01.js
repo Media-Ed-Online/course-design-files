9bf9f9e66c28b117af501d3a26b191c1c44b7fd2
JustineEvansUM <justine.evans@umontana.edu>
2021-08-30 02:58:40 -0600

    performance check

--- svma/art-sci-games/art-sci-games-01.js
+++ svma/art-sci-games/art-sci-games-01.js
@@ -17,13 +17,13 @@ $(document).ready(function() {
   var $backgroundShapes = $("<div class='bgShapes'><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></div>")
   $("#page").prepend($backgroundShapes);
 
-  $("div.bgShapes span").each(function() {
+  /*$("div.bgShapes span").each(function() {
     // Random BG Colors
     var bgShapesColorOpts = ["#DF3D9B", "#FDC273", "#0573CD"];
     var bgShapesColorRand = bgShapesColorOpts[Math.floor(Math.random() * bgShapesColorOpts.length)];
     console.log(bgShapesColorRand);
     $(this).css('background', bgShapesColorRand);
-  });
+  });*/
 });
 
 
