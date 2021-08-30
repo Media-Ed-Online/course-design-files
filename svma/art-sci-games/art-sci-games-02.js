7b47a5ee1fcdff071be5d8afdc24fe3abd12202e
JustineEvansUM <justine.evans@umontana.edu>
2021-08-30 14:44:22 -0600

    fix performance issues?

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -20,14 +20,12 @@
 function findElementByText(text) {
   var jSpot = $("p:contains(" + Level + ")")
     .filter(function() {
-      $(URL).each(function(index) {
         // variables
         let oldTitle = $(this).find(".snap-asset-link")
         let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
         // save the title, remove it, and add to new location
         oldTitle.remove();
         newTitle.append(oldTitle);
-      });
     })
     .parent(); // because you asked the parent of that element
 
