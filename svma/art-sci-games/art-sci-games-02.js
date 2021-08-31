bf8722d504fe718fe397f896fd4d8df1bbb68b33
JustineEvansUM <justine.evans@umontana.edu>
2021-08-31 11:51:31 -0600

    fix performance issues?

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -21,8 +21,22 @@ $(document).ready(function() {
   $("li.snap-asset .snap-asset-link").each(function() { //loop through the elements
     var elem = $(this); //reference to current
     var txt = $.trim(elem.text()); //get its text
+    var URL = "li.modtype_url"
+    var BOOK = "li.modtype_book"
+    var H5P = "li.modtype_h5pactivity"
     if (lookup[txt]) {
-      $("li.modtype_url").find(".snap-asset-link").remove()
+      var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+      var newIcon = ".snap-asset-content .contentafterlink div.tile-completion"
+      // first of set
+      $(this).each(function() {
+        // find the first of each, add empty class to mark
+        let oldIcon_URL = $(URL).first().find(oldIcon).addClass("moved")
+        let oldIcon_BOOK = $(BOOK).first().addClass("moved").find(oldIcon)
+        let oldIcon_H5P = $(H5P).first().addClass("moved").find(oldIcon)
+        let newIconLocation = $(URL).first().addClass("moved").find(newIcon)
+        // relocated all moved-targeted items to new location
+        newIconLocation.append(oldIcon_URL).append(oldIcon_H5P).append(oldIcon_BOOK);
+      });
     } else {
       lookup[txt] = true; //set the key in the lookup
     }
