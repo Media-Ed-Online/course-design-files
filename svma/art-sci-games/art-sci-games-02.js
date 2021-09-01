922bc78fccae55586aa0d0b36cacce30f9fbda30
JustineEvansUM <justine.evans@umontana.edu>
2021-09-01 14:41:16 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -4,19 +4,18 @@
 
 $(document).ready(function(index) {
   $(this).find('ul.topics li').each(function() {
-    if ($(this).find('p.instancename:contains("Level A"), p.instancename:contains("Level B")').length > 0) {
+    if ($(this).find('p.instancename:contains("Level A")').length > 0) {
       $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
       $(this).closest(".modtype_url").each(function() {
         let oldTitle = $(this).find(".snap-asset-link")
         let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
         oldTitle.remove();
         newTitle.append(oldTitle);
-
-        $(this).find(".modtype_book").each(function() {
-          var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-          var newIcon = ".snap-asset-content .contentafterlink div.tile-completion"
-          $("li.modtype_url").find(newIcon).append(oldIcon)
-        });
+      });
+      $(this).find(".modtype_book").each(function() {
+        var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        var newIcon = ".snap-asset-content .contentafterlink div.tile-completion"
+        $("li.modtype_url").find(newIcon).append(oldIcon)
       });
     } else {};
   });
