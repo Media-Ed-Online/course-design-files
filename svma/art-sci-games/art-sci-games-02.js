f31f4502a1a01b7ed82a5aab47f48fc9af4cc227
JustineEvansUM <justine.evans@umontana.edu>
2021-09-01 16:06:46 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -1,25 +1,6 @@
 /* ------------------
 | URL MODULE GROUP   |
 ------------------ */
-
-/*$(document).ready(function(index) {
-  $(this).find('ul.topics li li.modtype_url, ul.topics li li.modtype_book').each(function() {
-    if ($(this).find('p.instancename:contains("Level A")').length > 0) {
-      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
-      $(this).closest(".modtype_url").each(function() {
-        let oldTitle = $(this).find(".snap-asset-link")
-        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-        oldTitle.remove();
-        newTitle.append(oldTitle);
-      });
-
-      var oldIcon = $(this).closest(".modtype_book").find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-      var newIcon = $(this).closest(".modtype_book").find(".snap-asset-content .contentafterlink div.tile-completion")
-
-    } else {};
-  });
-});*/
-
 $(document).ready(function(index) {
   var instanceName = 'p.instancename:contains("Level A")'
   var URL = 'li.modtype_url'
@@ -35,14 +16,3 @@ $(document).ready(function(index) {
     });
   };
 });
-
-/*$(document).ready(function(index) {
-  var substr = ['p.instancename: contains("Level A")', 2, 3, 4];
-  $.each(substr, function(index, value) {
-    $(this).find(URL).add(BOOK).add(H5P).each(function() {
-      if ($(this).find(substr).length > 0) {
-        $(this).css("display", "none")
-      };
-    });
-  });
-});*/
