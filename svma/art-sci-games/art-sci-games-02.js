2114fcf0cdb477e2396a708cf120362e1fd1a1a7
JustineEvansUM <justine.evans@umontana.edu>
2021-09-01 17:38:22 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -27,8 +27,7 @@ $(document).ready(function(index) {
         });
 
         //3. -- move icons beneath new title home
-        $(URL).closest(BOOK).css("background","yellow")
-        });
+        $(URL).closest(BOOK).css("background", "yellow")
       };
     });
   };
