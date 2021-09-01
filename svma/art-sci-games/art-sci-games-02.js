0fa1df8ad0f4779fa346d802936ca5744023d359
JustineEvansUM <justine.evans@umontana.edu>
2021-08-31 20:21:20 -0600

    tiles js

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -1,25 +1,9 @@
 /* ------------------
 | URL MODULE GROUP   |
 ------------------ */
-jQuery.expr[':'].mcontains = function(obj, index, meta, stack){
-    result = false;
-    theList = meta[3].split("','");
+$(document).ready(function(index) {
 
-    var contents = (obj.textContent || obj.innerText || jQuery(obj).text() || '')
-
-    for (x=0;x<theList.length;x++) {
-        if (contents.indexOf(theList[x]) >= 0) {
-            return true;
-        }
-    }
-
-    return false;
-};
-
-/*$(document).ready(function(index) {
-  const tileTitles = ["Level A", "Level B"];
-
-  if ($('li.activity').find('p:contains("' + tileTitles + '")').length > 0) {
+  if ($('li.activity').find('p:contains("Level A"), p:contains("Level A")').length > 0) {
     $(this).find('.modtype_url').each(function(index) {
       let oldTitle = $(this).find(".snap-asset-link")
       let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
@@ -30,5 +14,4 @@ jQuery.expr[':'].mcontains = function(obj, index, meta, stack){
 
     }
   };
-});*/
-$("li:mcontains('Level A','Level B')").css("color","red")
+});
