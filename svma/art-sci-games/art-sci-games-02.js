5a10a32ff0bba0db13341f29fa3bc0f8639f3de0
JustineEvansUM <justine.evans@umontana.edu>
2021-09-01 16:04:10 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -3,46 +3,35 @@
 ------------------ */
 
 /*$(document).ready(function(index) {
-  $(this).find('ul.topics li li.modtype_url, ul.topics li li.modtype_book').each(function() {
-    if ($(this).find('p.instancename:contains("Level A")').length > 0) {
-      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
-      $(this).closest(".modtype_url").each(function() {
-        let oldTitle = $(this).find(".snap-asset-link")
-        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-        oldTitle.remove();
-        newTitle.append(oldTitle);
-      });
+$(this).find('ul.topics li li.modtype_url, ul.topics li li.modtype_book').each(function() {
+  if ($(this).find('p.instancename:contains("Level A")').length > 0) {
+    $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+    $(this).closest(".modtype_url").each(function() {
+      let oldTitle = $(this).find(".snap-asset-link")
+      let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+      oldTitle.remove();
+      newTitle.append(oldTitle);
+    });
 
-      var oldIcon = $(this).closest(".modtype_book").find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-      var newIcon = $(this).closest(".modtype_book").find(".snap-asset-content .contentafterlink div.tile-completion")
+    var oldIcon = $(this).closest(".modtype_book").find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+    var newIcon = $(this).closest(".modtype_book").find(".snap-asset-content .contentafterlink div.tile-completion")
 
-    } else {};
-  });
+  } else {};
+});
 });*/
 
 $(document).ready(function(index) {
-  var instanceName = 'p.instancename:contains("Level A")'
   var URL = 'li.modtype_url'
   var BOOK = 'li.modtype_book'
   var H5P = 'li.modtype_h5pactivity'
 
+  // Find items based on character set
   var s = 'ABCDE';
   for (var i = 0; i < s.length; i++) {
     $(this).find(URL).add(BOOK).add(H5P).each(function() {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
-        $(this).css("display", "none")
+        $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
       };
     });
   };
 });
-
-/*$(document).ready(function(index) {
-  var substr = ['p.instancename: contains("Level A")', 2, 3, 4];
-  $.each(substr, function(index, value) {
-    $(this).find(URL).add(BOOK).add(H5P).each(function() {
-      if ($(this).find(substr).length > 0) {
-        $(this).css("display", "none")
-      };
-    });
-  });
-});*/
