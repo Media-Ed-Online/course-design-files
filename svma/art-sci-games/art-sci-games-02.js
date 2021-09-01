3751c537dfc422d8075457c12bbcb46913e7f0a9
JustineEvansUM <justine.evans@umontana.edu>
2021-09-01 17:56:04 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -27,15 +27,14 @@ $(document).ready(function(index) {
         });
 
         //3. -- move icons beneath new title home
-        var lookedUp = {}
-
-        $(this).each(function() {
-          var text = $(this).text().trim();
-          if (lookedUp[text]) {
-            $(this).css("background", "yellow")
-          } else {
-            lookedUp[text] = true;
-          }
+        $(this).first(BOOK).each(function() {
+          // find the first of each, add empty class to mark
+          let oldIcon_URL = $(URL).first().find(oldIcon).addClass("moved")
+          let oldIcon_BOOK = $(BOOK).first().addClass("moved").find(oldIcon)
+          let oldIcon_H5P = $(H5P).first().addClass("moved").find(oldIcon)
+          let newIconLocation = $(URL).first().addClass("moved").find(newIcon)
+          // relocated all moved-targeted items to new location
+          newIconLocation.append(oldIcon_URL).append(oldIcon_H5P).append(oldIcon_BOOK);
         });
       };
     });
