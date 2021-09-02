ebf4c2492f08bf8ebfca6189f3f529e46ae6c7cd
JustineEvansUM <justine.evans@umontana.edu>
2021-09-02 11:03:53 -0600

    tiles

---
+++ svma/art-sci-games/art-sci-games-03.js
@@ -0,0 +1,55 @@
+/* ------------------
+| URL MODULE GROUP   |
+------------------ */
+$(document).ready(function(index) {
+  var instanceName = 'p.instancename:contains("Level A")'
+  var URL = 'li.modtype_url'
+  var BOOK = 'li.modtype_book'
+  var H5P = 'li.modtype_h5pactivity'
+
+  // FIND ITEMS BASED ON CHARACTER SET
+  var s = 'ABCDE';
+  for (var i = 0; i < s.length; i++) {
+    $(this).find(URL).add(BOOK).add(H5P).each(function() {
+      if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+
+        //-- 1. remove url overflow covering
+        $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+
+        //-- 2.1 move module title to new home
+        $(URL).each(function(index) {
+          if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+            // variables
+            let oldTitle = $(this).find(".snap-asset-link")
+            let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+            // save the title, remove it, and add to new location
+            oldTitle.remove();
+            newTitle.append(oldTitle);
+
+
+            //-- 2.2 move module icons to new home
+            var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+            var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
+            let oldIcon_URL = $(this).find(oldIcon)
+            let newIconLocation = $(this).find(newIcon)
+            oldIcon_URL.remove()
+            newIconLocation.append(oldIcon_URL)
+
+
+
+            /*$(BOOK).each(function(index) {
+              if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+                var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
+                var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
+                let oldIcon_URL = $(this).find(oldIcon)
+                let newIconLocation = $(this).parent().find(newIcon)
+                oldIcon_URL.remove()
+                newIconLocation.append(oldIcon_URL)
+              };
+            });*/
+          };
+        });
+      };
+    });
+  };
+});
