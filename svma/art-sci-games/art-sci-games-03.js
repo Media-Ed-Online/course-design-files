10c20fc8af07b2caeeb494561c98fae97d5a1c80
JustineEvansUM <justine.evans@umontana.edu>
2021-09-03 22:44:43 -0600

    tiles

--- svma/art-sci-games/art-sci-games-03.js
+++ svma/art-sci-games/art-sci-games-03.js
@@ -7,32 +7,8 @@ $(document).ready(function() {
   $('p.instancename').each(function() {
     var text = $(this).text().trim();
     if (lookedUp[text]) {
-      $(this).parentsUntil('li.activity').find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
 
-      //-- 2.1 move module title to new home
-      $('li.modtype_url').each(function(index) {
-
-        let oldTitle = $(this).find(".snap-asset-link")
-        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-        // save the title, remove it, and add to new location
-        oldTitle.remove();
-        newTitle.append(oldTitle);
-
-        var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-        var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
-        let oldIcon_URL = $(this).find(oldIcon)
-        let newIconLocation = $(this).find(newIcon)
-        oldIcon_URL.remove()
-        newIconLocation.append(oldIcon_URL)
-      });
-      $('li.modtype_book').each(function(index) {
-        var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
-        var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
-        let oldIcon_URL = $(this).find(oldIcon)
-        let newIconLocation = $(this).parent().find(newIcon)
-        oldIcon_URL.remove()
-        newIconLocation.append(oldIcon_URL)
-      });
     } else {
       lookedUp[text] = true;
     }
