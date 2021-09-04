338a83a18bd94485567b91e5d056748a5b674d75
JustineEvansUM <justine.evans@umontana.edu>
2021-09-03 20:58:05 -0600

    tiles

--- svma/art-sci-games/art-sci-games-03.js
+++ svma/art-sci-games/art-sci-games-03.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
    var lookedUp = {}
 
-   $('li').each(function() {
+   $('div').each(function() {
       var text = $(this).text().trim();
       if (lookedUp[text]) {
          $(this).css("background","yellow")
