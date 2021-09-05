a57d2c70aac4cebfe76f802505c95e3b2ecf9646
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 15:33:01 -0600

    tiles

--- svma/art-sci-games/art-sci-games-03.js
+++ svma/art-sci-games/art-sci-games-03.js
@@ -2,11 +2,24 @@
 | URL MODULE GROUP   |
 ------------------ */
 
-$('li.modtype_url').on('load', function() {
-  $('li.modtype_url').find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+var MY_SELECTOR = ".modtype_url" // Could be any selector
+
+var observer = new MutationObserver(function(mutations){
+  for (var i=0; i < mutations.length; i++){
+    for (var j=0; j < mutations[i].addedNodes.length; j++){
+      // We're iterating through _all_ the elements as the parser parses them,
+      // deciding if they're the one we're looking for.
+      if (mutations[i].addedNodes[j].matches(MY_SELECTOR)){
+        $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
+
+        // We found our element, we're done:
+        observer.disconnect();
+      };
+    }
+  }
 });
 
-
-// TARGET NEW & LOADED TOPICS
-
-// DO TO EACH
+observer.observe(document.documentElement, {
+  childList: true,
+  subtree: true
+});
