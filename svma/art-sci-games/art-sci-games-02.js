7d61c1a7fb133e3440d60bda8b8a5b55ef6b5eba
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 21:14:07 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -17,25 +17,25 @@ $(document).ready(function(index) {
         $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
 
         //-- 2.1 move module title to new home
-        $.each(function(index) {
-          if ($(this).find(URL).first()) {
-            // variables
-            let oldTitle = $(this).find(".snap-asset-link")
-            let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-            // save the title, remove it, and add to new location
-            oldTitle.remove();
-            newTitle.append(oldTitle);
+
+        if ($(this).find(URL).first()) {
+          // variables
+          let oldTitle = $(this).find(".snap-asset-link")
+          let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+          // save the title, remove it, and add to new location
+          oldTitle.remove();
+          newTitle.append(oldTitle);
 
 
-            //-- 2.2 move module icons to new home
-            var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
-            let oldIcon_URL = $(this).find(oldIcon)
-            let newIconLocation = $(this).find(newIcon)
-            oldIcon_URL.remove()
-            newIconLocation.append(oldIcon_URL)
-          };
-        });
+          //-- 2.2 move module icons to new home
+          var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+          var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
+          let oldIcon_URL = $(this).find(oldIcon)
+          let newIconLocation = $(this).find(newIcon)
+          oldIcon_URL.remove()
+          newIconLocation.append(oldIcon_URL)
+        };
+
       };
     });
   };
