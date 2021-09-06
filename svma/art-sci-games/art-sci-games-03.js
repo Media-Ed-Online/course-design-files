c6b6bc0bb6038dc0967f638c24aae41c93ae8b3b
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 18:01:15 -0600

    tiles

--- svma/art-sci-games/art-sci-games-03.js
+++ svma/art-sci-games/art-sci-games-03.js
@@ -9,10 +9,16 @@ $(document).ready(function(index) {
 
   // FIND ITEMS BASED ON CHARACTER SET
 
-  var urlContains = $(this).find('li.modtype_url p.instancename')
-  var bookContains = $(this).find('li.modtype_book p.instancename')
-  if (urlContains == bookContains) {
-    bookContains.remove()
-    urlContains.remove()
+  var s = 'ABCDE';
+  for (var i = 0; i < s.length; i++) {
+    $(this).find(URL).add(BOOK).add(H5P).each(function() {
+
+      var urlContains = $(this).find('li.modtype_url p.instancename:contains("Level ' + s.charAt(i) + '")')
+      var bookContains = $(this).find('li.modtype_book p.instancename:contains("Level ' + s.charAt(i) + '")')
+      if (urlContains == bookContains) {
+        bookContains.remove()
+        urlContains.remove()
+      };
+    });
   };
 });
