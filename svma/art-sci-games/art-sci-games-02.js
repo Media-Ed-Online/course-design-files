1b05a864b93a2930252272daa52e6c78176d12ef
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 19:53:49 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -10,44 +10,41 @@ $(document).ready(function(index) {
   // FIND ITEMS BASED ON CHARACTER SET
   var s = '1,2,3,4,5,6,7,8,9,10';
   for (var i = 0; i < s.length; i++) {
-    $(this).find(URL).add(BOOK).add(H5P).each(function() {
-      if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+    if ($(URL).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+      //-- 1. remove url overflow covering
+      $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
 
-        //-- 1. remove url overflow covering
-        $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
-
-        //-- 2.1 move module title to new home
-        $(URL).each(function(index) {
-          if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
-            // variables
-            let oldTitle = $(this).find(".snap-asset-link")
-            let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-            // save the title, remove it, and add to new location
-            oldTitle.remove();
-            newTitle.append(oldTitle);
+      //-- 2.1 move module title to new home
+      $(URL).each(function(index) {
+        if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+          // variables
+          let oldTitle = $(this).find(".snap-asset-link")
+          let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+          // save the title, remove it, and add to new location
+          oldTitle.remove();
+          newTitle.append(oldTitle);
 
 
-            //-- 2.2 move module icons to new home
-            var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
-            let oldIcon_URL = $(this).find(oldIcon)
-            let newIconLocation = $(this).find(newIcon)
-            oldIcon_URL.remove()
-            newIconLocation.append(oldIcon_URL)
+          //-- 2.2 move module icons to new home
+          var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+          var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
+          let oldIcon_URL = $(this).find(oldIcon)
+          let newIconLocation = $(this).find(newIcon)
+          oldIcon_URL.remove()
+          newIconLocation.append(oldIcon_URL)
 
-            $(BOOK).first().each(function(index) {
-              if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
-                var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
-                var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
-                let oldIcon_URL = $(this).find(oldIcon)
-                let newIconLocation = $(this).parent().first().find(newIcon)
-                oldIcon_URL.remove()
-                newIconLocation.append(oldIcon_URL)
-              };
-            });
-          };
-        });
-      };
-    });
+          $(BOOK).first().each(function(index) {
+            if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+              var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
+              var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
+              let oldIcon_URL = $(this).find(oldIcon)
+              let newIconLocation = $(this).parent().first().find(newIcon)
+              oldIcon_URL.remove()
+              newIconLocation.append(oldIcon_URL)
+            };
+          });
+        };
+      });
+    };
   };
 });
