4558300b2cdada1eac7c4ee56f36fd426673d145
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 21:26:20 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -10,15 +10,15 @@ $(document).ready(function(index) {
   // FIND ITEMS BASED ON CHARACTER SET
   var s = '123456789';
   for (var i = 0; i < s.length; i++) {
-    $(this).find(URL).add(BOOK).add(H5P).each(function() {
+    $(this).find(URL).each(function() {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
         //-- 1. remove url overflow covering
         $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
 
         //-- 2.1 move module title to new home
-        let oldTitle = $(URL).find(".snap-asset-link")
-        let newTitle = $(URL).find(".snap-asset-content .contentafterlink div.tile-title")
+        let oldTitle = $(this).find(".snap-asset-link")
+        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
         // save the title, remove it, and add to new location
         oldTitle.remove();
         newTitle.append(oldTitle);
@@ -27,7 +27,7 @@ $(document).ready(function(index) {
         var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
         var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
         let oldIcon_URL = $(BOOK).find(oldIcon)
-        let newIconLocation = $(URL).find(newIcon)
+        let newIconLocation = $(this).find(newIcon)
         oldIcon_URL.remove()
         newIconLocation.append(oldIcon_URL)
       };
