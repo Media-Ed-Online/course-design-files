da1248853ef19f213cd24eb8c0149e8913ab8bf3
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 01:44:20 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -41,13 +41,11 @@ $(document).ready(function(index) {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
         //-- 1. move first module icons to new home
-        let oldIcon = $(BOOK).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion")
+        let oldIcon = $(BOOK).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img").not('moved').first()
+        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").not('moved').first() 
         // save the icon, remove it, and add to new location
         oldIcon.first().remove()
         newIcon.first().append(oldIcon)
-        oldIcon.first().addClass('moved')
-        newIcon.first().addClass('moved')
 
         //-- 2. loop through modules and their icons
         $(this).each(function(index) {
