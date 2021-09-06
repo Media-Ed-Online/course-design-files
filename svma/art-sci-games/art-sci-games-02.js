f1da3920c9244cad75b3cdb4f32c077ded7cb81c
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 01:32:39 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -47,7 +47,7 @@ $(document).ready(function(index) {
             newIcon.append(oldIcon)
 
           } else {
-            let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+            let oldIcon = $(BOOK).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
             let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion")
             // save the icon, remove it, and add to new location
             oldIcon.remove()
