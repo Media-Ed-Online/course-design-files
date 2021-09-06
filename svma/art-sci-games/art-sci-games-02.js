5e684656c6f80aa09e436053d51068a28b31294c
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 21:23:18 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -17,17 +17,17 @@ $(document).ready(function(index) {
         $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
 
         //-- 2.1 move module title to new home
-        let oldTitle = $(this).find(".snap-asset-link")
-        let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+        let oldTitle = $(URL).find(".snap-asset-link")
+        let newTitle = $(URL).find(".snap-asset-content .contentafterlink div.tile-title")
         // save the title, remove it, and add to new location
         oldTitle.remove();
         newTitle.append(oldTitle);
 
         //-- 2.2 move module icons to new home
         var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-        var newIcon = $("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion")
-        let oldIcon_URL = $(this).find(oldIcon)
-        let newIconLocation = $(this).find(newIcon)
+        var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
+        let oldIcon_URL = $(BOOK).find(oldIcon)
+        let newIconLocation = $(URL).find(newIcon)
         oldIcon_URL.remove()
         newIconLocation.append(oldIcon_URL)
       };
