0e8879880cf661e837f6746cb2fa60b349482ac9
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 12:35:30 -0600

    get pages update

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -1,4 +1,23 @@
 /* ------------------
+| BACKGROUND         |
+------------------ */
+$(document).ready(function() {
+  // Draw Shapes
+  var $backgroundShapes = $("<div class='bgShapes'><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span><span></span></div>")
+  $("#page").prepend($backgroundShapes);
+
+  $("div.bgShapes span").each(function() {
+    // Random BG Colors
+    var bgShapesColorOpts = ["#DF3D9B", "#FDC273", "#0573CD"];
+    var bgShapesColorRand = bgShapesColorOpts[Math.floor(Math.random() * bgShapesColorOpts.length)];
+    console.log(bgShapesColorRand);
+    $(this).css('background', bgShapesColorRand);
+  });
+});
+
+
+
+/* ------------------
 | URL MODULE GROUP   |
 ------------------ */
 $(document).ready(function(index) {
