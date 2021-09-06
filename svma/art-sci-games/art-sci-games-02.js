53bcadf9126ca5ddfd5e1e3064a5559df1389144
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 02:00:55 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -32,6 +32,7 @@ $(document).ready(function(index) {
         oldIcon.remove()
         newIcon.append(oldIcon)
         oldIcon.addClass('moved')
+        newIcon.addClass('moved')
       };
     });
 
@@ -40,18 +41,19 @@ $(document).ready(function(index) {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
         //-- 1. move first module icons to new home
-        let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img").addClass('moved')
-        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion")
+        let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
         // save the icon, remove it, and add to new location
         oldIcon.remove()
-        newIcon.first().append(oldIcon)
-        newIcon.first().addClass('moved')
+        newIcon.append(oldIcon)
+        oldIcon.addClass('moved')
+        newIcon.addClass('moved')
 
         //-- 2. loop through modules and their icons
         $(this).each(function(index) {
           if ($(this).find(BOOK).not('.moved').first()) {
             let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").not('moved').first().addClass('moved')
+            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion:not('.moved')")
             // save the icon, remove it, and add to new location
             oldIcon.remove()
             newIcon.append(oldIcon)
