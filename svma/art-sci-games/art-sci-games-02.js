931cdbc5502a02b631c8a7bbe0cd90a51b46951d
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 01:18:30 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -40,24 +40,24 @@ $(document).ready(function(index) {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
         //-- 1. move first module icons to new home
-        let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
-        // save the icon, remove it, and add to new location
-        oldIcon.remove()
-        newIcon.append(oldIcon)
-        oldIcon.addClass('moved')
-        newIcon.addClass('moved')
+
 
         //-- 2. loop through modules and their icons
         $(this).each(function(index) {
           if ($(this).find(BOOK).not('.moved').first()) {
+            let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
+            // save the icon, remove it, and add to new location
+            oldIcon.remove()
+            newIcon.append(oldIcon)
+            oldIcon.addClass('moved')
+            newIcon.addClass('moved')
+          } else {
             let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
             let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion:not('.moved')")
             // save the icon, remove it, and add to new location
             oldIcon.remove()
             newIcon.append(oldIcon)
-          } else {
-            //  block of code to be executed if the condition is false
           }
         });
       };
