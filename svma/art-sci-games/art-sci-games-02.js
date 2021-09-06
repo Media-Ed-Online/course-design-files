ad2ef068e8cda9be334325395ec1f5525343e0cb
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 03:36:18 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -40,16 +40,21 @@ $(document).ready(function(index) {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
         //-- 1. move first module icons to new home
-        let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
-        // save the icon, remove it, and add to new location
-        oldIcon.remove()
-        newIcon.append(oldIcon)
-        oldIcon.addClass('moved')
-        newIcon.addClass('moved')
+        
 
         //-- 2. loop through modules and their icons
         $(this).each(function(index) {
+          if ($(this).find(BOOK).first()) {
+            let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
+            // save the icon, remove it, and add to new location
+            oldIcon.remove()
+            newIcon.append(oldIcon)
+            oldIcon.addClass('moved')
+            newIcon.addClass('moved')
+          } else {
+
+          };
           if ($(this).find(BOOK).next()) {
             let oldIcon = $(this).first().not('moved').find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img").addClass('moved')
             let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion:not('.moved')").first()
@@ -57,7 +62,7 @@ $(document).ready(function(index) {
             newIcon.append(oldIcon)
           } else {
             //
-          }
+          };
         });
       };
     });
