6cb1b30b517adc0638605f25f8c0f3a5dc6e9e37
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 00:01:32 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -46,9 +46,9 @@ $(document).ready(function(index) {
         newIcon.append(oldIcon)
         newIcon.addClass('moved')
 
-        //-- 2. loop through module's and their icons
-        $('li.modtype_book:not(.moved)').each(function(index) {
-          let oldIcon = $(BOOK).next().addClass('moved').find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        //-- 2. loop through modules and their icons
+        $(BOOK).each(function(index) {
+          let oldIcon = $('li.modtype_book:not(.moved)').first().addClass('moved').find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
           let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion:not(.moved)").next()
           // save the icon, remove it, and add to new location
           oldIcon.remove()
