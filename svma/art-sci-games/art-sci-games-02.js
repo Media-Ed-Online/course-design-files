8e3ddf7c673427ff7c6bc1edcb9823fc7587f14d
JustineEvansUM <justine.evans@umontana.edu>
2021-09-05 20:59:43 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -16,38 +16,32 @@ $(document).ready(function(index) {
         $(URL).first().ready(function(index) {
           //-- 1. remove url overflow covering
           $(this).find(".snap-asset-content .no-overflow").removeClass("no-overflow");
-        });
 
-        //-- 2.1 move module title to new home
-        $(URL).each(function(index) {
-          if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
-            // variables
-            let oldTitle = $(this).find(".snap-asset-link")
-            let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
-            // save the title, remove it, and add to new location
-            oldTitle.remove();
-            newTitle.append(oldTitle);
+          let oldTitle = $(this).find(".snap-asset-link")
+          let newTitle = $(this).find(".snap-asset-content .contentafterlink div.tile-title")
+          // save the title, remove it, and add to new location
+          oldTitle.remove();
+          newTitle.append(oldTitle);
 
 
-            //-- 2.2 move module icons to new home
-            var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
-            let oldIcon_URL = $(this).find(oldIcon)
-            let newIconLocation = $(this).find(newIcon)
-            oldIcon_URL.remove()
-            newIconLocation.append(oldIcon_URL)
+          //-- 2.2 move module icons to new home
+          var oldIcon = $(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+          var newIcon = $(".snap-asset-content .contentafterlink div.tile-completion")
+          let oldIcon_URL = $(this).find(oldIcon)
+          let newIconLocation = $(this).find(newIcon)
+          oldIcon_URL.remove()
+          newIconLocation.append(oldIcon_URL)
 
-            $(BOOK).first().each(function(index) {
-              if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
-                var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
-                var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
-                let oldIcon_URL = $(this).find(oldIcon)
-                let newIconLocation = $(this).parent().first().find(newIcon)
-                oldIcon_URL.remove()
-                newIconLocation.append(oldIcon_URL)
-              };
-            });
-          };
+          $(BOOK).first().each(function(index) {
+            if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
+              var oldIcon = ".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img"
+              var newIcon = "li.modtype_url .snap-asset-content .contentafterlink div.tile-completion"
+              let oldIcon_URL = $(this).find(oldIcon)
+              let newIconLocation = $(this).parent().first().find(newIcon)
+              oldIcon_URL.remove()
+              newIconLocation.append(oldIcon_URL)
+            };
+          });
         });
       };
     });
