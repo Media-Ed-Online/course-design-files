0f433d04b88bc596f361546926b5c175e05b68f5
JustineEvansUM <justine.evans@umontana.edu>
2021-09-06 01:35:56 -0600

    tiles

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -26,11 +26,12 @@ $(document).ready(function(index) {
         newTitle.append(oldTitle);
 
         //-- 3. move module icons to new home
-        let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img").addClass('moved')
-        let newIcon = $(this).find(".snap-asset-content .contentafterlink div.tile-completion").addClass('moved')
+        let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        let newIcon = $(this).find(".snap-asset-content .contentafterlink div.tile-completion")
         // save the icon, remove it, and add to new location
         oldIcon.remove()
         newIcon.append(oldIcon)
+        newIcon.addClass('moved')
       };
     });
 
@@ -38,22 +39,25 @@ $(document).ready(function(index) {
     $(this).find(BOOK).each(function(index) {
       if ($(this).find('p.instancename:contains("Level ' + s.charAt(i) + '")').length > 0) {
 
-        $(this).each(function(index) {
-          if ($(this).find(BOOK).not('.moved').next()) {
-            let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
-            // save the icon, remove it, and add to new location
-            oldIcon.remove()
-            newIcon.append(oldIcon)
+        //-- 1. move first module icons to new home
+        let oldIcon = $(BOOK).first().find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+        let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion").first()
+        // save the icon, remove it, and add to new location
+        oldIcon.remove()
+        newIcon.append(oldIcon)
+        oldIcon.addClass('moved')
+        newIcon.addClass('moved')
 
-          } else {
-            let oldIcon = $(BOOK).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
-            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion")
+        //-- 2. loop through modules and their icons
+        $(this).each(function(index) {
+          if ($(this).find(BOOK).not('.moved').first()) {
+            let oldIcon = $(this).find(".snap-header-card .snap-header-card-icons .snap-asset-completion-tracking img")
+            let newIcon = $(this).parentsUntil('li.section').find("li.modtype_url .snap-asset-content .contentafterlink div.tile-completion:not('.moved')")
             // save the icon, remove it, and add to new location
             oldIcon.remove()
             newIcon.append(oldIcon)
-            oldIcon.addClass('moved')
-            newIcon.addClass('moved')
+          } else {
+            //  block of code to be executed if the condition is false
           }
         });
       };
