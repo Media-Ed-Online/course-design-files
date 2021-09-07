67bc716136a6dfd30aac42281fad86c1724325dc
JustineEvansUM <justine.evans@umontana.edu>
2021-09-07 14:15:14 -0600

    tweaks

--- svma/art-sci-games/art-sci-games-02.js
+++ svma/art-sci-games/art-sci-games-02.js
@@ -1,7 +1,7 @@
 /* ------------------
 | URL MODULE GROUP   |
 ------------------ */
-$(document).ready(function(index) {
+$(this).ready(function(index) {
   var URL = 'li.modtype_url'
   var BOOK = 'li.modtype_book'
   var H5P = 'li.modtype_h5pactivity'
